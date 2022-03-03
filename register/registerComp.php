623dbbf57d70ebe28cb82df0a102892ce4098d71
Joyurizzz <65898468+Joyurizzz@users.noreply.github.com>
2022-03-03 12:58:15 +0800

    changes

---
+++ register/registerComp.php
@@ -0,0 +1,15 @@
+
+<?php 
+    session_start();
+    include './register-partials/header.php'
+?>
+
+<body>
+
+    <div class="reg-comp">
+    <h3>Registration Complete!</h3>
+    <p>Go back to <a href="login.php">Login</a></p>
+    </div>
+    
+</body>
+</html>
