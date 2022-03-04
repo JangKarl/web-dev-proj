d01f50efa0308c9f20b44eac44b9b6f3e85b7d0b
Shhbainu <arpilledabryan@gmail.com>
2022-03-05 00:10:17 +0800

    Fixing php ordering

--- foogs/cart.php
+++ foogs/cart.php
@@ -1,32 +1,32 @@
+<?php
+
+@include 'config.php';
+
+if(isset($_POST['update_update_btn'])){
+    $update_value = $_POST['update_quantity'];
+    $update_id = $_POST['update_quantity_id'];
+    $update_quantity_query = mysqli_query($conn, "UPDATE `cart` SET quantity = '$update_value' WHERE id = '$update_id'");
+    if($update_quantity_query){
+        header('location:cart.php');
+    };
+};
+
+if(isset($_GET['remove'])){
+    $remove_id = $_GET['remove'];
+    mysqli_query($conn, "DELETE FROM `cart` WHERE id = '$remove_id'");
+    header('location:cart.php');
+};
+
+if(isset($_GET['delete_all'])){
+    mysqli_query($conn, "DELETE FROM `cart`");
+    header('location:cart.php');
+}
+
+?>
+
 <?php include("partials/html-head.php") ?>
 
 <?php include("partials/navigation.php") ?>
-
-    <?php
-
-    @include 'config.php';
-
-    if(isset($_POST['update_update_btn'])){
-        $update_value = $_POST['update_quantity'];
-        $update_id = $_POST['update_quantity_id'];
-        $update_quantity_query = mysqli_query($conn, "UPDATE `cart` SET quantity = '$update_value' WHERE id = '$update_id'");
-        if($update_quantity_query){
-            header('location:cart.php');
-        };
-    };
-
-    if(isset($_GET['remove'])){
-        $remove_id = $_GET['remove'];
-        mysqli_query($conn, "DELETE FROM `cart` WHERE id = '$remove_id'");
-        header('location:cart.php');
-    };
-
-    if(isset($_GET['delete_all'])){
-        mysqli_query($conn, "DELETE FROM `cart`");
-        header('location:cart.php');
-    }
-
-    ?>
 
     <!-- container -->
     <div class="container">
--- foogs/products.php
+++ foogs/products.php
@@ -23,32 +23,7 @@ if(isset($_POST['add_to_cart'])){
 
 <?php include("partials/html-head.php") ?>
 
-
-    <!--Start of Navigation Bar-->
-    <div class="nav-bar">
-        <div class="logo">
-            <a href="index.php"><img src="images/icon.png" alt="icon.png" style="width: 100px;"></a>
-        </div>
-
-        <?php
-            @include 'config.php'; 
-            $select_rows = mysqli_query($conn, "SELECT * FROM `cart`") or die('query failed');
-            $row_count = mysqli_num_rows($select_rows);
-        ?>
-
-        <nav>
-            <ul>
-                <li><a href="index.php">Home</a></li>
-                <li><a href="products.php">Products</a></li>
-                <li><a href="about.php">About</a></li>
-                <li><a href="login.php">Login</a></li>
-
-                <li><a href="cart.php" class="fas fa-shopping-cart"> <span><?php echo $row_count; ?></span></a></li>
-                <li><a href="account.php" class="fas fa-user-circle"></a></li>
-            </ul>
-        </nav>
-    </div>
-    <!--End of Navigation Bar-->
+<?php include("partials/navigation.php") ?>
 
     
 
