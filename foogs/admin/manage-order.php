b7176b7de83091770eba6d5280390dd84fd06524
Shhbainu <arpilledabryan@gmail.com>
2022-03-05 02:37:20 +0800

    Update manage-order.php

--- foogs/admin/manage-order.php
+++ foogs/admin/manage-order.php
@@ -8,26 +8,20 @@
             <i class="fas fa-user-cog"></i>
         </div>
         <!-- Main body -->   
-        <div class="columns">
-            <h1>5</h1>
-            <br><br><br>
-            Vegetables
-        </div>
-        <div class="columns">
-            <h1>5</h1>
-            <br><br><br>
-            Fruits
-        </div>
-        <div class="columns">
-            <h1>5</h1>
-            <br><br><br>
-            Beverages
-        </div>
-        <div class="columns">
-            <h1>5</h1>
-            <br><br><br>
-            More
-        </div>
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     </div><!-- div for main-content -->        
 
