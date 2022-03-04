298eeae4df432f28be28dd8dc2fa3c7143b31178
Joyurizzz <65898468+Joyurizzz@users.noreply.github.com>
2022-03-04 18:20:13 +0800

    delete folder

--- web-dev(prof-ver.)/register/calling.php
+++
@@ -1,9 +0,0 @@
-<?php
-    session_start();
-
-    $conn = new mysqli('localhost', 'root', '', 'foogs_db');
-
-    $fN = 'select firstname from users where email = '.$_SESSION['email'].'';
-
-    $firstname = mysqli_query($conn, $fN);
-?>
